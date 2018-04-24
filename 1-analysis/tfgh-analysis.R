13a4213015c6b91abe692e80ef316992780ddc0a
Jade Benjamin-Chung <jadebc@gmail.com>
2018-04-24 10:42:46 -0700

    added robust se estimation

--- 1-analysis/tfgh-analysis.R
+++ 1-analysis/tfgh-analysis.R
@@ -6,7 +6,48 @@ rm(list=ls())
 library(dplyr)
 library(clusrank)
 library(tidyr)
+library(washb)
 load("~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/qdata.RData")
+
+
+#--------------------------------------
+# Estimate prevalence and 95%CI for each
+# test, robust sandwich SEs
+#--------------------------------------
+al.kk=washb_mean(qdata$alkk, id=qdata$clusterid, print = TRUE)
+hw.kk=washb_mean(qdata$hwkk, id=qdata$clusterid, print = TRUE)
+tt.kk=washb_mean(qdata$ttkk, id=qdata$clusterid, print = TRUE)
+
+al.q=washb_mean(qdata$positive.Al, id=qdata$clusterid, print = TRUE)
+na.q=washb_mean(qdata$positive.Na, id=qdata$clusterid, print = TRUE)
+ac.q=washb_mean(qdata$positive.Ac, id=qdata$clusterid, print = TRUE)
+ad.q=washb_mean(qdata$positive.Ad, id=qdata$clusterid, print = TRUE)
+tt.q=washb_mean(qdata$positive.Tt, id=qdata$clusterid, print = TRUE)
+
+#--------------------------------------
+# Estimate geometric mean and 95%CI for each
+# test, robust sandwich SEs
+#--------------------------------------
+qdata = qdata %>%
+  mutate(ln.alepg=log(alepg+1),
+         ln.hwepg=log(hwepg+1),
+         ln.ttepg=log(ttepg+1),
+         ln.CT.al=log(CTmean.Al+1),
+         ln.CT.na=log(CTmean.Na+1),
+         ln.CT.ac=log(CTmean.Ac+1),
+         ln.CT.ad=log(CTmean.Ad+1),
+         ln.CT.tt=log(CTmean.Tt+1))
+
+al.kk.gmn=washb_mean(qdata$ln.alepg, id=qdata$clusterid, print = TRUE)
+hw.kk.gmn=washb_mean(qdata$ln.hwepg, id=qdata$clusterid, print = TRUE)
+tt.kk.gmn=washb_mean(qdata$ln.ttepg, id=qdata$clusterid, print = TRUE)
+
+al.q.gmn=washb_mean(qdata$ln.CT.al, id=qdata$clusterid, print = TRUE)
+na.q.gmn=washb_mean(qdata$ln.CT.na, id=qdata$clusterid, print = TRUE)
+ac.q.gmn=washb_mean(qdata$ln.CT.ac, id=qdata$clusterid, print = TRUE)
+ad.q.gmn=washb_mean(qdata$ln.CT.ad, id=qdata$clusterid, print = TRUE)
+tt.q.gmn=washb_mean(qdata$ln.CT.tt, id=qdata$clusterid, print = TRUE)
+
 
 #--------------------------------------
 # Compare prevalence using 
@@ -63,3 +104,8 @@ Ac.corr <- cor.test(x=qdata$CTmean.Ac, y=qdata$hwkk,
 tt.corr <- cor.test(x=qdata$CTmean.Tt, y=qdata$ttepg, 
        method = 'spearman',exact=FALSE)
 
+save(al.kk,hw.kk,tt.kk,
+     al.q,na.q,ac.q,ad.q,tt.q,
+     al.kk.gmn,hw.kk.gmn,tt.kk.gmn,
+     al.q.gmn,na.q.gmn,ac.q.gmn,ad.q.gmn,tt.q.gmn,
+     file="~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/prev_results.RData")
