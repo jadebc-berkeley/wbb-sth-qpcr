147b0ce528a1f8c55409b498ae1368431fcb7556
Jade Benjamin-Chung <jadebc@gmail.com>
2019-02-06 13:07:17 -0800

    add header

--- 1-analysis/tfgh-descriptive.R
+++ 1-analysis/tfgh-descriptive.R
@@ -1,4 +1,10 @@
+#######################################
+# WASH Benefits Bangladesh STH KK qPCR validation
+# characteristics of study participants
+#######################################
 rm(list=ls())
+library(dplyr)
+
 load("~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/qdata.RData")
 
 prop.table(table(qdata$sex))
