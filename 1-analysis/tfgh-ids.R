ce375430865dc5e877afa6ccaf65681eab33ab5d
Jade Benjamin-Chung <jadebc@gmail.com>
2019-02-06 13:08:30 -0800

    new

---
+++ 1-analysis/tfgh-ids.R
@@ -0,0 +1,55 @@
+#######################################
+# WASH Benefits Bangladesh STH KK qPCR validation
+# Subset ids requested by Smith for
+# further analysis
+#######################################
+rm(list=ls())
+library(reshape2)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+load(qdata,file="~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/qdata.RData")
+
+#--------------------------------------
+# ids with positive ascaris kk and 
+# all negative for qPCR
+#--------------------------------------
+qdata$keep=ifelse(qdata$alepg>0 & qdata$positive.Ad==0 &
+                    qdata$positive.Na==0 & qdata$positive.Tt==0 & 
+                    qdata$positive.Al==0 & qdata$positive.Ac==0 &
+                    qdata$hwepg==0 & qdata$ttepg==0,1,0)
+
+keep=qdata[qdata$keep==1,]
+keep=keep[!is.na(keep$alint),]
+keep=keep[rev(order(keep$alepg)),]
+keep[1:6,c("dataid","personid")]
+keep[7:16,c("dataid","personid")]
+keep[17:29,c("dataid","personid")]
+
+# requested by Nils on 2/22/18
+# kk and qpcr positive for tt and nothing else
+qdata$keep2=ifelse(qdata$ttkk==1 & qdata$hwkk==0 & qdata$alkk==0 & 
+                     qdata$positive.Tt==1 & qdata$positive.Na!=1 & qdata$positive.Ad!=1 & 
+                     qdata$positive.Ss!=1 & qdata$positive.Al!=1 & qdata$positive.Ac!=1,1,0)
+as.matrix(paste0(qdata$dataid[qdata$keep2==1],"E",qdata$personid[qdata$keep2==1],"S1"))
+
+# kk and qpcr positive for Na and nothing else
+qdata$keep3=ifelse(qdata$ttkk==0 & qdata$hwkk==1 & qdata$alkk==0 & 
+                     qdata$positive.Tt!=1 & qdata$positive.Na==1 & qdata$positive.Ad!=1 & 
+                     qdata$positive.Ss!=1 & qdata$positive.Al!=1 & qdata$positive.Ac!=1,1,0)
+as.matrix(paste0(qdata$dataid[qdata$keep3==1],"E",qdata$personid[qdata$keep3==1],"S1"))
+
+# kk and qpcr positive for Al and nothing else
+qdata$keep4=ifelse(qdata$ttkk==0 & qdata$hwkk==0 & qdata$alkk==1 & 
+                     qdata$positive.Tt!=1 & qdata$positive.Na!=1 & qdata$positive.Ad!=1 & 
+                     qdata$positive.Ss!=1 & qdata$positive.Al==1 & qdata$positive.Ac!=1,1,0)
+as.matrix(paste0(qdata$dataid[qdata$keep4==1],"E",qdata$personid[qdata$keep4==1],"S1"))
+
+# kk and qpcr negative for all
+qdata$keep5=ifelse(qdata$ttkk==0 & qdata$hwkk==0 & qdata$alkk==0 & 
+                     qdata$positive.Tt!=1 & qdata$positive.Na!=1 & qdata$positive.Ad!=1 & 
+                     qdata$positive.Ss!=1 & qdata$positive.Al!=1 & qdata$positive.Ac!=1,1,0)
+as.matrix(paste0(qdata$dataid[qdata$keep5==1],"E",qdata$personid[qdata$keep5==1],"S1")[1:15])
+
+
---
+++ 2-fig-tab/tfgh-boxplot-al.R
@@ -0,0 +1,31 @@
+#######################################
+# WASH Benefits Bangladesh STH KK qPCR validation
+# boxplot of ascaris ct distribution
+# within intensity categories
+#######################################
+rm(list=ls())
+library(dplyr)
+library(tidyr)
+library(reshape2)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+load("~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/qdata.RData")
+
+qdata = qdata %>%
+  mutate(almh.f = ifelse(almh==1, "Moderate-heavy intensity\ninfection",
+                         "Light intensity\ninfection"))
+
+pdf(file="~/Dropbox/WASH-B-STH-Add-on/TFGH/Results/wbb-alconc-boxplot.pdf",
+    width=5,height=5)
+ggplot(qdata,aes(y=CTmean.Al,x=almh.f))+
+  geom_boxplot()+  
+  geom_dotplot(aes(fill=almh.f,col=almh.f),binaxis='y',stackdir='center',
+               stackratio=1,dotsize=0.7,binwidth=.008, alpha=0.5)+
+  xlab("Infection intensity")+
+  scale_y_log10(labels=seq(15,40,5), breaks=seq(15,40,5), limits=c(15, 40)) +
+  scale_color_manual(values=c("#CB59EB","#E37F2D"),guide=FALSE)+
+  scale_fill_manual(values=c("#CB59EB","#E37F2D"),guide=FALSE)+
+  ylab(expression(paste("log"[10], italic(" A. lumbricoides"), " Ct value")))+
+  theme_bw()
+dev.off()
