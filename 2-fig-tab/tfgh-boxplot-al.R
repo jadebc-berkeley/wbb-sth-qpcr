e9de8f377664e2eaa60faa082f1ef823bf024d71
Jade Benjamin-Chung <jadebc@gmail.com>
2019-02-13 16:43:12 -0800

    use al2, add t-test

--- 2-fig-tab/tfgh-boxplot-al.R
+++ 2-fig-tab/tfgh-boxplot-al.R
@@ -10,6 +10,7 @@ library(reshape2)
 library(ggplot2)
 library(grid)
 library(gridExtra)
+library(washb)
 load("~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/qdata.RData")
 
 qdata = qdata %>%
@@ -18,14 +19,25 @@ qdata = qdata %>%
 
 pdf(file="~/Dropbox/WASH-B-STH-Add-on/TFGH/Results/wbb-alconc-boxplot.pdf",
     width=5,height=5)
-ggplot(qdata,aes(y=CTmean.Al,x=almh.f))+
+ggplot(qdata,aes(y=CTmean.Al2,x=almh.f))+
   geom_boxplot()+  
   geom_dotplot(aes(fill=almh.f,col=almh.f),binaxis='y',stackdir='center',
-               stackratio=1,dotsize=0.7,binwidth=.008, alpha=0.5)+
+               stackratio=1,dotsize=0.9,binwidth=.5, alpha=0.5)+
   xlab("Infection intensity")+
-  scale_y_log10(labels=seq(15,40,5), breaks=seq(15,40,5), limits=c(15, 40)) +
-  scale_color_manual(values=c("#E69F00","#b35900"),guide=FALSE)+
-  scale_fill_manual(values=c("#E69F00","#b35900"),guide=FALSE)+
+  scale_color_manual(values=c("#F7CA65","#BD8302"),guide=FALSE)+
+  scale_fill_manual(values=c("#F7CA65","#BD8302"),guide=FALSE)+
   ylab(expression(paste(italic(" A. lumbricoides"), " Ct value")))+
   theme_bw()
 dev.off()
+
+
+# p-value for difference in means t test
+qdata = qdata %>% filter(!is.na(CTmean.Al2))
+
+washb_ttest(Y=qdata$CTmean.Al2,tr=qdata$almh.f,strat=qdata$block, 
+            contrast=c("Light intensity\ninfection","Moderate-heavy intensity\ninfection"))
+
+# quantiles of the Ct values within infection
+# intensity levels
+quantile(qdata$CTmean.Al2[qdata$almh.f=="Light intensity\ninfection"], prob=c(0,.5,1))
+quantile(qdata$CTmean.Al2[qdata$almh.f=="Moderate-heavy intensity\ninfection"], prob=c(0,.5,1))
