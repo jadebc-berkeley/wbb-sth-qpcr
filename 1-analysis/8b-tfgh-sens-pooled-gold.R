ed8a85026f390d340af10bf9f07f01cc5ae2efc1
Jade Benjamin-Chung <jadebc@gmail.com>
2019-02-13 16:16:53 -0800

    new

---
+++ 1-analysis/8b-tfgh-sens-pooled-gold.R
@@ -0,0 +1,79 @@
+#######################################
+# WASH Benefits Bangladesh STH KK qPCR validation
+# estimate sensitivity, specificity
+# of kk using either method as gold
+#######################################
+rm(list=ls())
+library(dplyr)
+library(washb)
+load("~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/qdata.RData")
+
+# subset to relevant columns
+d = qdata %>%
+  select(dataid,personid,clusterid,positive.Al2,positive.Hw,
+         positive.Tt,alkk,hwkk,ttkk)
+
+#----------------------------------------
+# calculate sensitivity
+#----------------------------------------
+ald1= d %>% filter(positive.Al2==1) %>%
+  filter(!is.na(alkk)) %>%
+  filter(!is.na(positive.Al2)) 
+al.kk.sens=washb_mean(ald1$alkk,id=ald1$clusterid)
+al.q.sens=washb_mean(ald1$positive.Al2,id=ald1$clusterid)
+
+hwd1= d %>% filter(hwkk==1 | positive.Hw==1) %>%
+  filter(!is.na(hwkk)) %>%
+  filter(!is.na(positive.Hw)) 
+hw.kk.sens=washb_mean(hwd1$hwkk,id=hwd1$clusterid)
+hw.q.sens=washb_mean(hwd1$positive.Hw,id=hwd1$clusterid)
+
+ttd1= d %>% filter(ttkk==1 | positive.Tt==1) %>%
+  filter(!is.na(ttkk)) %>%
+  filter(!is.na(positive.Tt)) 
+tt.kk.sens=washb_mean(ttd1$ttkk,id=ttd1$clusterid)
+tt.q.sens=washb_mean(ttd1$positive.Tt,id=ttd1$clusterid)
+
+kk.sens.pooled.gold=data.frame(rbind(al.kk.sens,hw.kk.sens,tt.kk.sens))
+kk.sens.pooled.gold$org=c("A. lumbricoides","Hookworm","T. trichiura")
+
+q.sens.pooled.gold=data.frame(rbind(al.q.sens,hw.q.sens,tt.q.sens))
+q.sens.pooled.gold$org=c("A. lumbricoides","Hookworm","T. trichiura")
+
+#----------------------------------------
+# calculate specificity
+#----------------------------------------
+ald2= d %>% filter(positive.Al2==0) %>%
+  filter(!is.na(alkk)) %>%
+  filter(!is.na(positive.Al2)) %>% 
+  mutate(alkkneg=ifelse(alkk==1,0,1),
+         alqneg = ifelse(positive.Al2==1, 0, 1))
+al.kk.spec=washb_mean(ald2$alkkneg,id=ald2$clusterid)
+al.q.spec=washb_mean(ald2$alqneg,id=ald2$clusterid)
+
+hwd2= d %>% filter(positive.Hw==0 | hwkk==0) %>%
+  filter(!is.na(hwkk)) %>%
+  filter(!is.na(positive.Hw)) %>%
+  mutate(hwkkneg=ifelse(hwkk==1,0,1),
+         hwqneg = ifelse(positive.Hw==1,0,1))
+hw.kk.spec=washb_mean(hwd2$hwkkneg,id=hwd2$clusterid)
+hw.q.spec=washb_mean(hwd2$hwqneg,id=hwd2$clusterid)
+
+ttd2= d %>% filter(positive.Tt==0 | ttkk==0) %>%
+  filter(!is.na(ttkk)) %>%
+  filter(!is.na(positive.Tt)) %>%
+  mutate(ttkkneg=ifelse(ttkk==1,0,1),
+         ttqneg=ifelse(positive.Tt==1,0,1))
+tt.kk.spec=washb_mean(ttd2$ttkkneg,id=ttd2$clusterid)
+tt.q.spec=washb_mean(ttd2$ttqneg,id=ttd2$clusterid)
+
+kk.spec.pooled.gold=data.frame(rbind(al.kk.spec,hw.kk.spec,tt.kk.spec))
+kk.spec.pooled.gold$org=c("A. lumbricoides","Hookworm","T. trichiura")
+
+q.spec.pooled.gold=data.frame(rbind(al.q.spec,hw.q.spec,tt.q.spec))
+q.spec.pooled.gold$org=c("A. lumbricoides","Hookworm","T. trichiura")
+
+save(kk.sens.pooled.gold, kk.spec.pooled.gold,
+     q.sens.pooled.gold, q.spec.pooled.gold,
+     file="~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/sensspec_pooled_gold.RData")
+
---
+++ 2-fig-tab/tfgh-table-sens-spec-pooled-gold.R
@@ -0,0 +1,43 @@
+#######################################
+# WASH Benefits Bangladesh STH KK qPCR validation
+# estimate sensitivity, specificity
+# using pooled kk qPCR as gold standard
+#######################################
+rm(list=ls())
+library(dplyr)
+
+load("~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/sensspec_pooled_gold.RData")
+load("~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/qdata.RData")
+
+source("~/Documents/CRG/wash-benefits/bangladesh/src/wbb-sth-qpcr/2-fig-tab/0-base-table-functions.R")
+
+N.al=nrow(qdata[!is.na(qdata$positive.Al2) & !is.na(qdata$alkk),])
+N.hw=nrow(qdata[!is.na(qdata$positive.Hw) & !is.na(qdata$hwkk),])
+N.tt=nrow(qdata[!is.na(qdata$positive.Tt) & !is.na(qdata$ttkk),])
+
+kk.sens=kk.sens.pooled.gold %>%
+  select(org, Mean, lb=Lower.95.CI, ub=Upper.95.CI) %>%
+  mutate(sens=ptestci.format(Mean,lb,ub,decimals=0,scale=100)) %>%
+  select(org,sens)
+
+kk.spec=kk.spec.pooled.gold %>%
+  select(org, Mean, lb=Lower.95.CI, ub=Upper.95.CI) %>%
+  mutate(spec=ptestci.format(Mean,lb,ub,decimals=0,scale=100)) %>%
+  select(org,spec)
+
+q.sens=q.sens.pooled.gold %>%
+  select(org, Mean, lb=Lower.95.CI, ub=Upper.95.CI) %>%
+  mutate(sens=ptestci.format(Mean,lb,ub,decimals=0,scale=100)) %>%
+  select(org,sens)
+
+q.spec=q.spec.pooled.gold %>%
+  select(org, Mean, lb=Lower.95.CI, ub=Upper.95.CI) %>%
+  mutate(spec=ptestci.format(Mean,lb,ub,decimals=0,scale=100)) %>%
+  select(org,spec)
+
+out=full_join(kk.sens,kk.spec,by=c("org"))
+out=full_join(out,q.sens,by=c("org"))
+out=full_join(out,q.spec,by=c("org"))
+out$N=c(N.al,N.hw,N.tt)
+
+write.csv(out,file="~/Dropbox/WASH-B-STH-Add-on/TFGH/Results/ss_pooled_gold.csv",row.names=FALSE)
