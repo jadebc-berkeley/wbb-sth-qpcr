1c89acb3c416d3d17e61d4ec30804684a692a7a7
Jade Benjamin-Chung <jadebc@gmail.com>
2019-04-03 16:13:41 -0700

    update with remaining al results

--- 2-fig-tab/tfgh-plot-venn-diagram.R
+++ 2-fig-tab/tfgh-plot-venn-diagram.R
@@ -9,6 +9,40 @@ library(venneuler)
 
 load("~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/qdata.RData")
 
+# get n's - KK
+get_kk_n = function(data, al, hw, tt){
+  x = data %>% filter(
+    alkk == al,
+    hwkk == hw, 
+    ttkk == tt
+  ) %>%
+    summarise(n = n())
+  return(as.numeric(x))
+}
+
+# get n's - qPCR
+get_q_n = function(data, al, hw, tt){
+  x = data %>% filter(
+    positive.Al2 == al,
+    positive.Hw == hw, 
+    positive.Tt == tt
+  ) %>%
+    summarise(n = n())
+  return(as.numeric(x))
+}
+
+# get n's - qPCR Hw
+get_hw_n = function(data, ac, na, ad){
+  x = data %>% filter(
+    positive.Ac == ac,
+    positive.Na == na, 
+    positive.Ad == ad
+  ) %>%
+    summarise(n = n())
+  return(as.numeric(x))
+}
+
+
 # ---------------------------------------
 # kato katz plot
 # ---------------------------------------
@@ -16,14 +50,14 @@ kk=qdata[,c("alkk","hwkk","ttkk")]
 kk$coinf=rowSums(kk,na.rm=TRUE)
 
 # intersections
-alhw=nrow(kk[kk$alkk==1 & kk$hwkk==1 & kk$ttkk==0,])
-hwtt=nrow(kk[kk$alkk==0 & kk$hwkk==1 & kk$ttkk==1,])
-altt=nrow(kk[kk$alkk==1 & kk$hwkk==0 & kk$ttkk==1,])
-alhwtt=nrow(kk[kk$alkk==1 & kk$hwkk==1 & kk$ttkk==1,])
+alhw=get_kk_n(kk, al = 1, hw = 1, tt = 0)
+hwtt=get_kk_n(kk, al = 0, hw = 1, tt = 1)
+altt=get_kk_n(kk, al = 1, hw = 0, tt = 1)
+alhwtt=get_kk_n(kk, al = 1, hw = 1, tt = 1)
 
-altot=nrow(kk[kk$alkk==1 & kk$hwkk==0 & kk$ttkk==0,])
-hwtot=nrow(kk[kk$alkk==0 & kk$hwkk==1 & kk$ttkk==0,])
-tttot=nrow(kk[kk$alkk==0 & kk$hwkk==0 & kk$ttkk==1,])
+altot=get_kk_n(kk, al = 1, hw = 0, tt = 0)
+hwtot=get_kk_n(kk, al = 0, hw = 1, tt = 0)
+tttot=get_kk_n(kk, al = 0, hw = 0, tt = 1)
 
 # circles proportional to size 
 v <- venneuler(c(Ascaris=altot, Hookworm=hwtot, Trichuris=tttot,
@@ -55,26 +89,26 @@ pdf(file="~/Dropbox/WASH-B-STH-Add-on/TFGH/Results/wbb-venn-kk.pdf",
   text(x=v$centers[3,1]+0.06,y=v$centers[3,2]+0.02, labels=paste(tttot))
   
   # intersections
-  text(x=v$centers[1,1]+0.21,y=v$centers[1,2]+0.08, labels=paste(altt))
+  text(x=v$centers[1,1]+0.23,y=v$centers[1,2]+0.07, labels=paste(altt))
   text(x=v$centers[1,1]+0.18,y=v$centers[1,2]-0.13, labels=paste(alhw))
-  text(x=v$centers[1,1]+0.22,y=v$centers[1,2]-0.02, labels=paste(alhwtt))
-  text(x=v$centers[1,1]+0.32,y=v$centers[1,2]-0.04, labels=paste(hwtt))
+  text(x=v$centers[1,1]+0.23,y=v$centers[1,2]-0.035, labels=paste(alhwtt))
+  text(x=v$centers[1,1]+0.32,y=v$centers[1,2]-0.047, labels=paste(hwtt))
 dev.off()
 
 # ---------------------------------------
 # qpcr plot
 # ---------------------------------------
 # counts
-altot=nrow(qdata[qdata$positive.Al2==1 & qdata$positive.Hw==0 & qdata$positive.Tt==0,])
-hwtot=nrow(qdata[qdata$positive.Al==0 & qdata$positive.Hw==1 & qdata$positive.Tt==0,])
-tttot=nrow(qdata[qdata$positive.Al==0 & qdata$positive.Hw==0 & qdata$positive.Tt==1,])
+altot=get_q_n(data = qdata, al = 1, hw = 0, tt = 0)
+hwtot=get_q_n(data = qdata, al = 0, hw = 1, tt = 0)
+tttot=get_q_n(data = qdata, al = 0, hw = 0, tt = 1)
 
 # intersections
-alhw=nrow(qdata[qdata$positive.Al==1 & qdata$positive.Hw==1 & qdata$positive.Tt==0,])
-altt=nrow(qdata[qdata$positive.Al==1 & qdata$positive.Hw==0 & qdata$positive.Tt==1,])
-hwtt=nrow(qdata[qdata$positive.Al==0 & qdata$positive.Hw==1 & qdata$positive.Tt==1,])
+alhw=get_q_n(data = qdata, al = 1, hw = 1, tt = 0)
+altt=get_q_n(data = qdata, al = 1, hw = 0, tt = 1)
+hwtt=get_q_n(data = qdata, al = 0, hw = 1, tt = 1)
 
-alhwtt=nrow(qdata[qdata$positive.Al2==1 & qdata$positive.Hw==1 & qdata$positive.Tt==1,])
+alhwtt=get_q_n(data = qdata, al = 1, hw = 1, tt = 1)
 
 qpcr <- venneuler(c(Ascaris=altot, Hookworm=hwtot, Trichuris=tttot,
                  "Ascaris&Hookworm"=alhw,"Hookworm&Trichuris"=hwtt,
@@ -87,20 +121,20 @@ pdf(file="~/Dropbox/WASH-B-STH-Add-on/TFGH/Results/wbb-venn-qpcr.pdf",
     width=6,height=6)
   plot(qpcr, col=c(cb.lightorange, cb.blue,cb.green))
   # titles
-  text(x=centers[1,1]+0.15,y=centers[1,2]+0.25, labels=expression(paste(italic("A. lumbricoides"))))
-  text(x=centers[2,1]-0.20,y=centers[2,2]+0.25, labels="Hookworm")
-  text(x=centers[3,1]-0.19,y=centers[3,2]-0.19, labels=expression(paste(italic("T. trichiura"))))
+  text(x=centers[1,1]+0.0,y=centers[1,2]+0.26, labels=expression(paste(italic("A. lumbricoides"))))
+  text(x=centers[2,1]-0.2,y=centers[2,2]-0.22, labels="Hookworm")
+  text(x=centers[3,1]-0.12,y=centers[3,2]+0.2, labels=expression(paste(italic("T. trichiura"))))
   
   # non intersections
-  text(x=centers[1,1]+0.10,y=centers[1,2]-0.02, labels=paste(altot))
-  text(x=centers[2,1]-0.01,y=centers[2,2]+0.05, labels=paste(hwtot))
-  text(x=centers[3,1]-0.02,y=centers[3,2]-0.09, labels=paste(tttot))
+  text(x=centers[1,1]+0.04,y=centers[1,2]+0.08, labels=paste(altot))# done
+  text(x=centers[2,1]-0.01,y=centers[2,2]-0.1, labels=paste(hwtot))# done
+  text(x=centers[3,1]-0.08,y=centers[3,2]+0.02, labels=paste(tttot))# done
   
   # intersections
-  text(x=centers[1,1]-0.10,y=centers[1,2]-0.12, labels=paste(altt))
-  text(x=centers[1,1]-0.16,y=centers[1,2]-0.02, labels=paste(alhwtt))
-  text(x=centers[3,1]-0.07,y=centers[3,2]+0.07, labels=paste(hwtt))
-  text(x=centers[1,1]-0.1,y=centers[1,2]+0.10, labels=paste(alhw))
+  text(x=centers[1,1]-0.16,y=centers[1,2]+0.02, labels=paste(altt))
+  text(x=centers[1,1]-0.13,y=centers[1,2]-0.1, labels=paste(alhwtt)) # done
+  text(x=centers[3,1]-0.02,y=centers[3,2]-0.1, labels=paste(hwtt))
+  text(x=centers[1,1]+0.02,y=centers[1,2]-0.15, labels=paste(alhw)) # done
 dev.off()
 
 
@@ -108,15 +142,16 @@ dev.off()
 # qpcr hw plot
 # ---------------------------------------
 # counts
-actot=nrow(qdata[qdata$positive.Ac==1 & qdata$positive.Na==0 & qdata$positive.Ad==0,])
-natot=nrow(qdata[qdata$positive.Ac==0 & qdata$positive.Na==1 & qdata$positive.Ad==0,])
-adtot=nrow(qdata[qdata$positive.Ac==0 & qdata$positive.Na==0 & qdata$positive.Ad==1,])
+actot=get_hw_n(qdata, ac = 1, na = 0, ad = 0)
+natot=get_hw_n(qdata, ac = 0, na = 1, ad = 0)
+adtot=get_hw_n(qdata, ac = 0, na = 0, ad = 1)
 
 # intersections
-acna=nrow(qdata[qdata$positive.Ac==1 & qdata$positive.Na==1 & qdata$positive.Ad==0,])
-acad=nrow(qdata[qdata$positive.Ac==1 & qdata$positive.Na==0 & qdata$positive.Ad==1,])
-naad=nrow(qdata[qdata$positive.Ac==0 & qdata$positive.Na==1 & qdata$positive.Ad==1,])
-acnaad=nrow(qdata[qdata$positive.Ac==1 & qdata$positive.Na==1 & qdata$positive.Ad==1,])
+acna=get_hw_n(qdata, ac = 1, na = 1, ad = 0)
+acad=get_hw_n(qdata, ac = 1, na = 0, ad = 1)
+naad=get_hw_n(qdata, ac = 0, na = 1, ad = 1)
+
+acnaad=get_hw_n(qdata, ac = 1, na = 1, ad = 1)
 
 qpcr.hw <- venneuler(c("A. ceylanicum"=actot, 
                     "N. americanus"=natot, 
@@ -141,6 +176,6 @@ text(x=centers[3,1]-0.08,y=centers[3,2]+0.04, labels=expression(paste(italic("A.
 text(x=centers[1,1],y=centers[1,2]+0.03, labels=paste(actot))
 text(x=centers[2,1],y=centers[2,2]-0.02, labels=paste(natot))
 # intersections
-text(x=centers[1,1]+0.01,y=centers[1,2]-0.08, labels=paste(acna))
-text(x=centers[1,1]-0.2,y=centers[1,2]-0.2, labels=paste(naad),cex=0.75)
+text(x=centers[1,1]+0.0,y=centers[1,2]-0.09, labels=paste(acna))
+text(x=centers[1,1]-0.245,y=centers[1,2]-0.165, labels=paste(naad),cex=0.75)
 dev.off()
