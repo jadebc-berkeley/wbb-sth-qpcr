16c3581976e32862691ae972522876fdaf1f50b5
Jade Benjamin-Chung <jadebc@gmail.com>
2019-04-05 13:05:01 -0700

    add missing script

--- 1-analysis-scripts/0-run-analysis.sh
+++ 1-analysis-scripts/0-run-analysis.sh
@@ -8,6 +8,7 @@ rm -f 4-tfgh-perm-test.Rout
 rm -f 6-tfgh-sens-qpcr-gold.Rout
 rm -f 7-tfgh-sens-pooled-gold.Rout
 rm -f 8-tfgh-lca-inputs.Rout
+rm -f 10-tfgh-preliminary.Rout
 
 R CMD BATCH 1-tfgh-preliminary.R
 R CMD BATCH 2-tfgh-descriptive.R
@@ -16,3 +17,4 @@ R CMD BATCH 4-tfgh-perm-test.R
 R CMD BATCH 6-tfgh-sens-qpcr-gold.R
 R CMD BATCH 7-tfgh-sens-pooled-gold.R
 R CMD BATCH 8-tfgh-lca-inputs.R
+R CMD BATCH 10-tfgh-preliminary.R
