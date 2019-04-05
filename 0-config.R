dd4cd713f92ae4ca7e3871b641be92aa22efd918
Jade Benjamin-Chung <jadebc@gmail.com>
2019-04-05 11:28:52 -0700

    new

---
+++ 0-config.R
@@ -0,0 +1,18 @@
+#######################################
+# WASH Benefits Bangladesh STH KK qPCR validation
+
+# configure data directories
+# source base functions
+# load libraries
+#######################################
+library(here)
+library(ggplot2)
+library(dplyr)
+
+# define directories
+fig_dir = paste0(here::here(),"/3-figures/")
+
+# source base functions  
+source(paste0(here::here(), "/2-fig-tab-scripts/0-base-plot-functions.R"))
+source(paste0(here::here(), "/2-fig-tab-scripts/0-base-plot-functions.R"))
+
