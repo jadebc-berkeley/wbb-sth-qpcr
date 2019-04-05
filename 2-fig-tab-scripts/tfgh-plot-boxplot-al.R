cb8edc27e0e504082d62c71d276c384dceaf5d8d
Jade Benjamin-Chung <jadebc@gmail.com>
2019-04-05 12:32:27 -0700

    update font size

--- 2-fig-tab-scripts/tfgh-plot-boxplot-al.R
+++ 2-fig-tab-scripts/tfgh-plot-boxplot-al.R
@@ -4,20 +4,17 @@
 # within intensity categories
 #######################################
 rm(list=ls())
-library(dplyr)
-library(tidyr)
-library(reshape2)
-library(ggplot2)
-library(grid)
-library(gridExtra)
-library(washb)
+
+# configure directories, load libraries and base functions
+source(paste0(here::here(), "/0-config.R"))
+
 load("~/Dropbox/WASH-B-STH-Add-on/TFGH/Data/RData/qdata.RData")
 
 qdata = qdata %>%
   mutate(almh.f = ifelse(almh==1, "Moderate-heavy intensity\ninfection",
                          "Light intensity\ninfection"))
 
-pdf(file="~/Dropbox/WASH-B-STH-Add-on/TFGH/Results/wbb-alconc-boxplot.pdf",
+pdf(file=paste0(fig_dir,"wbb-alconc-boxplot.pdf"),
     width=5,height=5)
 ggplot(qdata,aes(y=CTmean.Al2,x=almh.f))+
   geom_boxplot()+  
@@ -27,7 +24,15 @@ ggplot(qdata,aes(y=CTmean.Al2,x=almh.f))+
   scale_color_manual(values=c("#F7CA65","#BD8302"),guide=FALSE)+
   scale_fill_manual(values=c("#F7CA65","#BD8302"),guide=FALSE)+
   ylab(expression(paste(italic(" Ascaris lumbricoides"), " Cq value")))+
-  theme_bw()
+  theme_bw() +
+
+# customize font size, legend
+theme(legend.text.align = 0,
+      axis.text.x = element_text(size=12),
+      axis.title.x = element_text(size=14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+      axis.text.y = element_text(size=12),
+      axis.title.y = element_text(size=14, margin = margin(t = 0, r = 10, b = 0, l = 0))) 
+  
 dev.off()
 
 
