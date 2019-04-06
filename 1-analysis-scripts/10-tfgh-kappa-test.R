9b9cbce858e692ef39becf340e083df15ff538f5
Jade Benjamin-Chung <jadebc@gmail.com>
2019-04-06 08:18:53 -0700

    remove old ascaris assay

--- 1-analysis-scripts/10-tfgh-kappa-test.R
+++ 1-analysis-scripts/10-tfgh-kappa-test.R
@@ -14,9 +14,9 @@ source(paste0(here::here(), "/0-config.R"))
 # load data
 load(paste0(data_dir,"qdata.RData"))
 
-qdata = qdata %>% filter(!is.na(positive.Al2))
+qdata = qdata %>% filter(!is.na(positive.Al))
 
-al.kappa = kappa2(qdata[,c("alkk","positive.Al2")], "unweighted")
+al.kappa = kappa2(qdata[,c("alkk","positive.Al")], "unweighted")
 hw.kappa = kappa2(qdata[,c("hwkk","positive.Hw")], "unweighted")
 tt.kappa = kappa2(qdata[,c("ttkk","positive.Tt")], "unweighted")
 
