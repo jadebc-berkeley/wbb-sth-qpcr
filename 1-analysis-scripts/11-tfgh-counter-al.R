d4848c90e4482bc59f0825bb53f18693aa47d4a5
Jade Benjamin-Chung <jadebc@gmail.com>
2019-10-17 09:08:57 -0700

    use individual technician name instead of combo of names for pair that reviewed slide

--- 1-analysis-scripts/11-tfgh-counter-al.R
+++ 1-analysis-scripts/11-tfgh-counter-al.R
@@ -16,14 +16,22 @@ load(paste0(data_dir,"qdata.RData"))
 
 # create indicator for false positive Ascaris in KK
 qdata = qdata %>% mutate(
-  false_al = ifelse(alkk==1 & positive.Al==0,1,0),
-  counter = as.factor(counter)
+  false_al = ifelse(alkk==1 & positive.Al==0,1,0)
 )
 
+# since each slide was read by two counters, 
+# need to transform data so that it is in long 
+# format
+counter_data = qdata %>% dplyr::select(counter1, counter2, false_al) %>%
+  gather("counter","name",-false_al) %>%
+  mutate(name = as.factor(name))
+
 # plot P(False positive | counter)
-false_pos_counter = lapply(as.list(levels(qdata$counter)), function(x) mean_se(qdata$false_al[qdata$counter==x]))
+false_pos_counter = lapply(as.list(levels(counter_data$name)), function(x)
+  mean_se(counter_data$false_al[counter_data$name==x]))
 false_pos_counter = bind_rows(false_pos_counter) %>%
-  mutate(Counter = levels(qdata$counter))
+  mutate(Counter = levels(counter_data$name))
+
 
 false_plot = ggplot(false_pos_counter, aes(x = Counter, y = y)) + 
   geom_bar(stat="identity",width=0.5, aes(fill=Counter)) +
@@ -34,19 +42,19 @@ false_plot = ggplot(false_pos_counter, aes(x = Counter, y = y)) +
   theme(legend.position="none")
 
 ggsave(false_plot, file = paste0(fig_dir, "wbb-qpcr-kk-falsepos-counter.pdf"),
-       width = 6, height = 3)
+       width = 4, height = 3)
 
 # test association
-glm_fit = glm(false_al ~ counter, data = qdata, family = "binomial")
+glm_fit = glm(false_al ~ counter, data = counter_data, family = "binomial")
 summary(glm_fit)
 
-x = qdata %>% group_by(labdate, counter) %>%
-  summarise(falsepos = mean(false_al))
-
-ggplot(x, aes(x = labdate, y = falsepos, group=counter))+
-  # geom_point(aes(col=counter)) +
-  geom_line(aes(col=counter))
-
+# x = qdata %>% group_by(labdate, counter) %>%
+#   summarise(falsepos = mean(false_al))
+# 
+# ggplot(x, aes(x = labdate, y = falsepos, group=counter))+
+#   # geom_point(aes(col=counter)) +
+#   geom_line(aes(col=counter))
+# 
 
 
 
